f87a4f1e3e61554a7138fd8c339c4e0bed174456
Shuxin-Wang <81904920+A-BigTree@users.noreply.github.com>
2023-04-21 00:06:30 +0800

    update User Handler

---
+++ src/main/java/booking/handler/UserHandler.java
@@ -0,0 +1,38 @@
+/**
+ * ==================================================
+ * Project: seu_hotel_Booking
+ * Package: booking.handler
+ * =====================================================
+ * Title: UserHandler.java
+ * Created: [2023/4/20 17:31] by Shuxin-Wang
+ * =====================================================
+ * Description: description here
+ * =====================================================
+ * Revised History:
+ * 1. 2023/4/20, created by Shuxin-Wang.
+ * 2.
+ */
+
+package booking.handler;
+
+import booking.service.api.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserHandler {
+    private UserService userService;
+
+    public UserHandler(UserService userService){
+        this.userService = userService;
+    }
+
+    @RequestMapping("/user/login")
+    public @ResponseBody String loginUser(Model model){
+
+
+        return "";
+    }
+}
